f7423b6ed7ae377fbd00825691877a2944328112
박준수 <tony180322@gmail.com>
2021-03-09 00:37:15 +0900

    fix: edit ServerApplication.java for using Tomcat

--- src/main/java/com/hercules/server/ServerApplication.java
+++ src/main/java/com/hercules/server/ServerApplication.java
@@ -2,9 +2,16 @@ package com.hercules.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ServerApplication {
+public class ServerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ServerApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
