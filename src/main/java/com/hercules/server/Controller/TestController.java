196c15b5cc8a63f1aa52f8ee7a6931cd794cea99
박준수 <tony180322@gmail.com>
2021-03-09 00:00:24 +0900

    fix : edit test code

--- src/main/java/com/hercules/server/Controller/TestController.java
+++ src/main/java/com/hercules/server/Controller/TestController.java
@@ -1,7 +1,9 @@
 package com.hercules.server.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class TestController {
     @GetMapping("/test")
     public String test() {
